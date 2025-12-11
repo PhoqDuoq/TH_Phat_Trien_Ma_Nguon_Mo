2621949b9ac11d59b19938b2cbe63f53c50c2dfe
Phong <phong123@gmail.com>
2025-12-11 09:00:50 +0700

    Them code Nodejs va file CI/CD

---
+++ index.js
@@ -0,0 +1,2 @@
+echo 'const express = require("express"); const app = express(); app.get("/", (req, res) => res.send("Bai Tap 1: CI/CD Thanh Cong!")); app.listen(3000, () => console.log("Server ready"));' > index.js
+
